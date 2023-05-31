e180b25c9a64e1e0b0135db2598925bbd398fd9e
Caelmab <barrylamb530@gmail.com>
2023-05-31 15:21:59 -0600

    16 commit

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from '../card/Card.jsx';
+import styles from "../cards/cards.module.css";
 
 const Cards = ({ dogs }) => {
   if(!dogs || dogs.length === 0) {
     return <div>No se encontraron resultados</div>
   }
   return (
-    <div>
+    <div className={styles['card-list']}>
       {dogs?.map((dog) => (
         <Card key={dog.id} dog={dog} />
       ))}
